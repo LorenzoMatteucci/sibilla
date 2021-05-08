f2b806a5ecb231cd7857280f993df3776519b14e
LorenzoMatteucci <80180571+LorenzoMatteucci@users.noreply.github.com>
2021-05-08 03:37:24 +0200

    Update RuleGeneratorSBML.java
    added some methods

--- langs/pm/src/main/java/it/unicam/quasylab/sibilla/langs/pm/sbml/RuleGeneratorSBML.java
+++ langs/pm/src/main/java/it/unicam/quasylab/sibilla/langs/pm/sbml/RuleGeneratorSBML.java
@@ -1,11 +1,15 @@
 package it.unicam.quasylab.sibilla.langs.pm.sbml;
 
+import it.unicam.quasylab.sibilla.core.models.EvaluationEnvironment;
+import it.unicam.quasylab.sibilla.core.models.pm.Population;
 import it.unicam.quasylab.sibilla.core.models.pm.PopulationRule;
 import it.unicam.quasylab.sibilla.core.models.pm.util.PopulationRegistry;
 import it.unicam.quasylab.sibilla.langs.pm.ExpressionEvaluator;
 import org.sbml.jsbml.ListOf;
 import org.sbml.jsbml.Reaction;
+import org.sbml.jsbml.SpeciesReference;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.function.Function;
@@ -13,7 +17,9 @@ import java.util.function.Function;
 public class RuleGeneratorSBML {
 
     private PopulationRegistry registry;
-    private ExpressionEvaluator expressionEvaluator;
+    private EvaluationEnvironment evaluationEnvironment;
+
+    //private ExpressionEvaluator expressionEvaluator;
     private HashMap<String, Double> context;
 
     private List<PopulationRule> rules;
@@ -23,11 +29,63 @@ public class RuleGeneratorSBML {
     ListOf<Reaction> reactionList;
 
 
-    public Function<Double,Double> prova(){
-        return null;
+    public RuleGeneratorSBML(PopulationRegistry registry, EvaluationEnvironment evaluationEnvironment) {
+        this.registry = registry;
+        this.evaluationEnvironment = evaluationEnvironment;
+    }
+
+    public List<PopulationRule> getRulesFromReactionList(ListOf<Reaction> reactionList){
+        ArrayList<PopulationRule> populationRuleList= new ArrayList<>();
+        for (Reaction r : reactionList) {
+            populationRuleList.add(getRuleFromReaction(r));
+        }
+        return populationRuleList;
     }
 
 
+
+    /*
+            PopulationRule rule_S_G_A = new ReactionRule(
+                "S->A",
+                new Population[] { new Population(S), new Population(A)} ,
+                new Population[] { new Population(G), new Population(A)},
+                (t,s) -> (1-PROB_ASINT)*s.getOccupancy(S)* PROB_TRANSMISSION*LAMBDA_MEET *(s.getOccupancy(A)/N));
+
+     */
+
+
+
+
+    /*
+    	public ReactionRule(String name, Population[] reactants, Population[] products, RatePopulationFunction rateFunction) {
+		this(name,null,reactants,products,rateFunction);
+	}
+     */
+
+    private PopulationRule getRuleFromReaction(Reaction r){
+        String name = getRuleName(r);
+
+        return null;
+    }
+
+    private String getRuleName(Reaction r){
+        String name = "";
+        ListOf<SpeciesReference> reactantsList = r.getListOfReactants();
+        ListOf<SpeciesReference> productsList = r.getListOfProducts();
+        for (SpeciesReference reactant : reactantsList) name += reactant.getSpecies() +"_";
+        name += name.substring(0, name.length() - 1) + " --> ";
+        for (SpeciesReference product : productsList) name += product.getSpecies() +"_";
+        return name.substring(0, name.length() - 1);
+    }
+
+    private Population[] getReactionElement(ListOf<SpeciesReference> reactionElement){
+
+        for (SpeciesReference re:reactionElement) {
+
+        }
+        
+        return null;
+    }
 
     /*
     @Override
